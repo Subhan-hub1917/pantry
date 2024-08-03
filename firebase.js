dae6238b0bf74094c93abf1bc3b19c83df86ef51
Subhan Qamar <subiq1917@gmail.com>
2024-08-03 08:09:29 +0500

    Firebase Errro Resolved

--- firebase.js
+++ firebase.js
@@ -1,25 +1,35 @@
-"use client"
-// Import the functions you need from the SDKs you need
+"use client";
+import { useEffect } from 'react';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// import { getFirestore } from "firebase/firebase-firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAE9xvHOabnAUwzMkna_aO-V0zrgisLkS0",
-  authDomain: "pantry-items-ba87a.firebaseapp.com",
-  projectId: "pantry-items-ba87a",
-  storageBucket: "pantry-items-ba87a.appspot.com",
-  messagingSenderId: 352151066454,
-  appId: "1:352151066454:web:750ff1db58ecade52d28f7",
-  measurementId: " G-1J60GL96FR"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_api_Key,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_measurementId
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db=getFirestore(app);
+let analytics;
+let db;
+const initializeFirebase = () => {
+  const app = initializeApp(firebaseConfig);
+  if (typeof window !== "undefined") {
+    analytics = getAnalytics(app);
+  }
+  db = getFirestore(app);
+};
+
+export const useFirebase = () => {
+  useEffect(() => {
+    initializeFirebase();
+  }, []);
+
+  return { db, analytics };
+};
